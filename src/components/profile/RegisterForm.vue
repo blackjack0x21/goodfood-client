e79e2c88310a830a7a1f0401496d740121e4ee47
Blackjack <bl4ckj4ck.contact@gmail.com>
2022-05-08 23:05:02 +0200

    CLI-58 Barre résistance mdp

--- src/components/profile/RegisterForm.vue
+++ src/components/profile/RegisterForm.vue
@@ -16,10 +16,12 @@
             <ion-item>
                 <ion-label position="floating">Mot de passe</ion-label>
                 <ion-input type="password" required v-model="password"/>
+                <password-meter :password="password" />
             </ion-item>
             <ion-item>
                 <ion-label position="floating">Confirmation mot de passe</ion-label>
-                <ion-input type="password" required v-model="passwordConfimation"/>
+                <ion-input type="password" required v-model="passwordConfirmation"/>
+                <password-meter :password="passwordConfirmation" />
             </ion-item>
         </ion-list>            
         <ion-label v-if="errorMessage != ''" position="floating" style="color:#C20000">{{errorMessage}}</ion-label>
@@ -33,6 +35,8 @@
 <script>
 import { IonLabel, IonItem, IonNavLink, IonButton, IonInput, IonList  } from "@ionic/vue";
 import axios from 'axios';
+import PasswordMeter from 'vue-simple-password-meter';
+import zxcvbn from 'zxcvbn';
 
 export default {
   components: {
@@ -42,32 +46,50 @@ export default {
       IonButton,
       IonInput,
       IonList,
+      PasswordMeter
   },
 
   methods: {
     async registerHandler() {
-      try {
-        // Send a POST request
-        await axios({
-          headers: {
-            "Content-Type" : "application/json",
-            "Access-Control-Allow-Origin": "*"
-          },
-          method: 'post',
-          url: 'http://localhost:3000/api/customer',
-          data: {
-            firstName: this.firstName,
-            lastName: this.lastName,
-            password: this.passwordConfirmation,
-            passwordConfirmation: this.passwordConfirmation,
-            email: this.email
-          }
-        });
+      if(this.formValid() === true) {
+        try {
+          // Send a POST request
+          await axios({
+            headers: {
+              "Content-Type" : "application/json",
+              "Access-Control-Allow-Origin": "*"
+            },
+            method: 'post',
+            url: 'http://localhost:3000/api/customer',
+            data: {
+              firstName: this.firstName,
+              lastName: this.lastName,
+              password: this.passwordConfirmation,
+              passwordConfirmation: this.passwordConfirmation,
+              email: this.email
+            }
+          });
+        }
+        catch(e) {
+          console.log(e);
+        }
       }
-      catch(e) {
-        console.log(e);
+    },
+
+    formValid() {
+        let isValid = true;
+        if(this.password !== this.passwordConfirmation) {
+          console.log("passwords do not match");
+          isValid = false;
+        }
+
+        let passwordStrength = zxcvbn(this.password).score;
+        if(passwordStrength < 2) {
+          console.log("password too weak : score is : " + passwordStrength);
+          isValid = false
+        }
+        return isValid;
       }
-    }
   },
 
   data() {
