a77d0edce919d4269d0e0fabd77b937c80ea6b75
clem <clement.mutter@gmail.com>
2022-05-14 18:28:37 +0200

    CLI-65 création de la page détail pizza
    Création de la page détail pizza et ajout  dans une session storage

--- src/components/PizzaItem.vue
+++ src/components/PizzaItem.vue
@@ -1,24 +1,28 @@
 <template>
+  <ion-nav-link
+    router-link="/pizzadetails/"
+  >
     <ion-card>
-    <ion-card-header>
-        <ion-img :src="pizzaImage"></ion-img>
-        <ion-card-title>{{name}}</ion-card-title>
-    </ion-card-header>
-
-    <ion-card-content>{{price}}</ion-card-content>
-  </ion-card>
+      <ion-card-header>
+          <ion-img :src="pizzaImage"></ion-img>
+          <ion-card-title>{{name}}</ion-card-title>
+      </ion-card-header>
+      <ion-card-content>{{price}}</ion-card-content>
+    </ion-card>
+  </ion-nav-link>
 </template>
 
 <script lang="ts">
-import { IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonImg } from '@ionic/vue';
+import { IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonImg, IonNavLink } from '@ionic/vue';
 import '../styles/pizzaitem.css';
 
 export default {
     props: {
+      pizzaId: String,
       pizzaImage: String,
       name: String,
       price: String  
     },
-    components: { IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonImg }
+    components: { IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonImg, IonNavLink }
 }
 </script>
---
+++ src/views/PizzaDetailsPage.vue
@@ -0,0 +1,38 @@
+<template>
+  <base-layout pageTitle="Détails pizza" page-default-back-link="/tabs/tab1">
+    <ion-img src='https://www.pngplay.com/wp-content/uploads/6/Real-Dominos-Pizza-Transparent-PNG.png'></ion-img>
+      <h1 text-align="center" id="text-pizza"><strong> Bacon </strong></h1>
+      <h2 text-align="center" id="desc-pizza">La pizza est une recette traditionnelle issue de la cuisine italienne. Trouvant son origine à Naples, 
+          elle est préparée à base d’une galette de pâte à pain remplie de divers mélanges d’ingrédients avant d’être cuite au four.</h2>
+    <ion-button shape="round" expand="block" @click="addToBasketHandler"> {{ $t('Ajouter au panier') }} </ion-button> 
+  </base-layout>
+</template>
+
+<script lang="ts">
+import { IonButton, IonImg} from '@ionic/vue';
+import '../styles/pizzadetailspage.css';
+import notification, { TypeNotification } from '../../utils/notification';
+import { startLoading, stopLoading } from '../../utils/loader';
+export default {
+    components: {
+        IonImg,
+        IonButton,
+    },
+    
+    methods: {
+      async addToBasketHandler() {
+        try {
+          await startLoading("Ajout au panier en cours");
+          sessionStorage.setItem('clé', 'numéro');
+          notification("Pizza ajoutée au panier", TypeNotification.Success);
+        }
+        catch(error) {
+          notification("Échec de l'ajout au panier", TypeNotification.Danger);
+        }
+        finally {
+          stopLoading();
+        }
+      }
+    }
+}
+</script>
