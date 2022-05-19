976f5e92ff23ffe443473ce91b24f8f164ff2c2f
clem <clement.mutter@gmail.com>
2022-05-19 15:02:16 +0200

    CLI-19

---
+++ src/views/BasketPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <base-layout pageTitle="Panier">
+      <template v-slot:actions-start>
+          <ion-fab-button class="button-left">
+            <ion-img src="https://hatscripts.github.io/circle-flags/flags/fr.svg"></ion-img>
+          </ion-fab-button>
+      </template>
+
+    </base-layout>
+</template>
+
+<script lang="ts">
+import '../styles/index.css'
+
+export default {
+    
+}
+    
+</script>
---
+++ src/views/LegalMention.vue
@@ -0,0 +1,25 @@
+<template>
+<base-layout pageTitle="Restaurant">
+      <template v-slot:actions-start>
+          <ion-fab-button class="button-left">
+            <ion-img src="https://hatscripts.github.io/circle-flags/flags/fr.svg"></ion-img>
+          </ion-fab-button>
+      </template>
+      <template v-slot:actions-end>
+          <ion-fab-button class="button-right">
+            <ion-icon :icon="cartSharp"></ion-icon>
+          </ion-fab-button>
+          </template>
+  </base-layout>
+</template>
+
+<script>
+
+    import '../styles/index.css'
+    import { IonIcon, IonImg } from '@ionic/vue';
+   
+    export default { IonIcon, IonImg }
+    
+    
+
+</script>
